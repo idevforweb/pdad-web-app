bf35c2c2622dedb525a2483319953e1aae88a800
webdevhead1 <webdevhead1@pop-os.localdomain>
2024-03-26 22:32:22 -0400

    component work

--- frontend/public/js/web-components/mycomponent.js
+++ frontend/public/js/web-components/mycomponent.js
@@ -31,18 +31,20 @@ class DataTable extends HTMLElement {
 
     this.api(url)
 
-      .then((data) => {
-        function gridContainer(colName, index) {
+      .then((apiResponseData) => {
+        console.log(apiResponseData);
+
+        function dataColumn(arr, headerName, columnName, index) {
           return `
-        <div class="grid-container">
-          <div class="col-name col-${Object.keys(data[0])}">
-            ${colName}
-          </div>
-            ${data[index][colName].map((number) => `<div class="item">${number}</div>`).join('')}
-        </div>`;
+          <div class="grid-column">
+            <div class="col-name col-${Object.keys(apiResponseData[0])}">
+              ${headerName}
+            </div>
+              ${arr[index][columnName].map((number) => `<div class="item">${number}</div>`).join('')}
+          </div>`;
         }
 
-        if (data[0].Month.length == 0) {
+        if (apiResponseData[0].Month.length == 0) {
           this.innerHTML = `
           <div class="wrong-input">
             There is no date matching ${this.date}, please try again.
@@ -51,17 +53,23 @@ class DataTable extends HTMLElement {
         }
 
         this.innerHTML = `
-          ${gridContainer('Month', 0)}
-          ${gridContainer('Day', 1)}
-          ${gridContainer('Year', 2)}
-          ${gridContainer('Number 1', 3)}
-        
-        
+        <div class="grid-data">
+          ${dataColumn(apiResponseData, 'MN', 'Month', 0)}
+          ${dataColumn(apiResponseData, 'DY', 'Day', 1)}
+          ${dataColumn(apiResponseData, 'YR', 'Year', 2)}
+          ${dataColumn(apiResponseData, 'N1', 'Number 1', 3)}
+          ${dataColumn(apiResponseData, 'N2', 'Number 2', 4)}
+          ${dataColumn(apiResponseData, 'N3', 'Number 3', 5)}
+          ${dataColumn(apiResponseData, 'N4', 'Number 4', 6)}
+          ${dataColumn(apiResponseData, 'N5', 'Number 5', 7)}
+          ${dataColumn(apiResponseData, 'PB', 'Power Ball', 8)}
+          ${dataColumn(apiResponseData, 'PP', 'Power Play', 9)}
+        </div>
         `;
       })
 
       .catch((error) => {
-        this.innerHTML = `An error has occured.`;
+        this.innerHTML = `Couldn't connect to API.`;
       });
   }
 
